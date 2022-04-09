a30a0c6b19ea2c1500d13b83a9068065795c286f
okansari <sari.okan@gmail.com>
2022-04-09 16:00:04 +0300

    son son son son

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
