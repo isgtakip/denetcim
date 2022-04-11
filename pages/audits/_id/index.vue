41a0c6141d2cabd96f3a773ba6665f4e2386476a
okansari <sari.okan@gmail.com>
2022-04-12 00:26:40 +0300

    audit_index refresh problem

--- pages/audits/_id/index.vue
+++ pages/audits/_id/index.vue
@@ -4,9 +4,14 @@
 <script>
 /*eslint-disable*/
 export default {
-     async asyncData({ params, $denetcimApi }) {
-      const audit_form = await $denetcimApi.$get(`audits/${params.id}`)
+    /* async asyncData({ params, $denetcimApi }) {
+      const audit_form = await $denetcimApi.$get(`audits/`+params.id)
       return {audit_form}
-    },
+    },*/
+    async asyncData({env,params,$axios}) {
+        console.log(env.apiURL)
+      const audit_form = await $axios.$get(env.apiURL+`/audits/${params.id}`)
+      return { audit_form }
+    }
 }
 </script>
